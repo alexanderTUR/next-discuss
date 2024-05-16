287e775bc95857b32fbae222e44c7a1a4b9c32c6
Oleksandr Tur <alexandertur.fe@gmail.com>
2024-05-16 14:04:33 +0200

    Add Create Post form

--- src/components/posts/post-create-form.tsx
+++ src/components/posts/post-create-form.tsx
@@ -12,41 +12,49 @@ import {
 import * as actions from "@/actions";
 import FormButton from "@/components/common/form-button";
 
-export default function TopicCreateForm() {
-  const [formState, action] = useFormState(actions.createTopic, {
-    errors: {},
-  });
+interface PostCreateFormProps {
+  slug: string;
+}
+
+export default function PostCreateForm({ slug }: PostCreateFormProps) {
+  const [formState, action] = useFormState(
+    actions.createPost.bind(null, slug),
+    {
+      errors: {},
+    },
+  );
+
   return (
     <Popover placement="left">
       <PopoverTrigger>
-        <Button color="primary">Create a Topic</Button>
+        <Button color="primary">Create a Post</Button>
       </PopoverTrigger>
       <PopoverContent>
         <form action={action}>
           <div className="flex flex-col gap-4 p-4 w-80">
-            <h3 className="text-lg">Create a Topic</h3>
+            <h3 className="text-lg">Create a Post</h3>
             <Input
-              name="name"
-              label="Name"
+              name="title"
+              label="Title"
               labelPlacement="outside"
               placeholder="Name"
-              isInvalid={!!formState.errors.name}
-              errorMessage={formState.errors.name?.join(", ")}
+              isInvalid={!!formState.errors.title}
+              errorMessage={formState.errors.title?.join(", ")}
             />
             <Textarea
-              name="description"
-              label="Description"
+              name="content"
+              label="Content"
               labelPlacement="outside"
-              placeholder="Descrive your topic"
-              isInvalid={!!formState.errors.description}
-              errorMessage={formState.errors.description?.join(", ")}
+              placeholder="Content"
+              isInvalid={!!formState.errors.content}
+              errorMessage={formState.errors.content?.join(", ")}
             />
             {formState.errors._form && (
               <div className="p-2 bg-red-200 border border-red-400 rounded">
                 {formState.errors._form.join(", ")}
               </div>
             )}
-            <FormButton>Create</FormButton>
+            <FormButton>Create Post</FormButton>
           </div>
         </form>
       </PopoverContent>
